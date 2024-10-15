33634fd9c3541605f28931acddaf2e95284b428c
asadnokhan786 <asadnokhan@outlook.com>
2024-10-15 16:44:45 -0600

    Setup + publish commit

---
+++ tests/test_zip.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import pytest
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from zip import unzip, unzip_all
+
+
+def test_zip_simple():
+    pass
+
+def test_zip_incalid():
+    pass
+
+def test_zip_all_simple():
+    pass
---
+++ zip.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import zipfile
+
+
+def zip(src, dst):
+    pass
+
+def zip_all(src, dst):
+    pass
