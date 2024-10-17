bb1939cade2bb123420beb4ec97790da8f791938
asadnokhan786 <asadnokhan@outlook.com>
2024-10-17 17:46:50 -0600

    Added function behavior descriptions

--- unzip.py
+++ unzip.py
@@ -3,6 +3,7 @@ import os
 import zipfile
 
 
+# Unzips a file from src to dst with a folder name of the file name from src
 def unzip(src, dst):
     with zipfile.ZipFile(src, 'r') as zip_ref:
         file_name_with_ext = os.path.basename(src)
@@ -10,6 +11,7 @@ def unzip(src, dst):
         dst_path = os.path.join(dst, file_name)
         zip_ref.extractall(dst_path)
 
+# Unzips all files from src to dst recursively
 def unzip_all(src, dst):
     for root, dirs, files in os.walk(src):
         if root != src:
--- zip.py
+++ zip.py
@@ -4,6 +4,8 @@ import zipfile
 import imghdr
 
 
+# Zips a directory from unzip_dir to zip_dir with a filename that is the same as the folder name of unzip_dir
+# Excludes files that are not valid image formats
 def zip(unzip_dir, zip_dir):
     base_folder_name = os.path.basename(unzip_dir.rstrip('/'))
     zip_filename = f"{base_folder_name}.cbz"
@@ -21,10 +23,13 @@ def zip(unzip_dir, zip_dir):
                 zip_file.write(full_path, os.path.basename(file))
 
 
+# Zips all directories from src to dst recursively
+# Folder is deemed "zippable" if it contains at least one valid image file
 def zip_all(src, dst):
     pass
 
 
+# Determines if a given file is a valid image file
 def is_valid_image(file_path):
     valid_formats = {'jpeg', 'png', 'webp', 'bmp', 'gif', 'tiff'}
     image_type = imghdr.what(file_path)
