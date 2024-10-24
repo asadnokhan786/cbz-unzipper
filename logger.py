1df33f5070df7c58ecfa879d6671966c355a6867
asadnokhan786 <asadnokhan@outlook.com>
2024-10-23 23:44:42 -0600

    Got logging working
    Created logger object that can be used by all modules in project

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+logger = logging.getLogger('cbz-unzipper-logger')
+logger.setLevel(logging.DEBUG)
+
+handler = RotatingFileHandler('logs/cbz_unzipper_debug.log', maxBytes=5000000, backupCount=5)
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import argparse
 import os
 from unzip import unzip_all
 from zip import zip_all
+from logger import logger
 
 
 def main():
+    logger.debug('Awaiting command line parsing')
     parser = argparse.ArgumentParser(prog='cbz-unzipper CLI',
                                      description='cbz-unzipper CLI is a command line interface program implemented in python\
                                                   aimed at unzipping/zipping files in mass safely and reliably to the user\'s specific needs')
@@ -13,6 +15,7 @@ def main():
     parser.add_argument('--mode', '-m', required='true', type=str, help='Mode to specify unzipping or zipping')
 
     args = parser.parse_args()
+    logger.debug('Successfully parsed command line arguments')
 
     base_dir = os.path.abspath(os.path.dirname(__file__))
     src = os.path.join(base_dir, args.src)
@@ -23,9 +26,11 @@ def main():
         raise ValueError(f"{src} is NOT a valid source path")
     
     if mode == "unzip":
+        logger.debug('Running unzip_all from main')
         unzip_all(src, dst)
 
     if mode == 'zip':
+        logger.debug('Running zip_all from main')
         zip_all(src, dst)
 
 
