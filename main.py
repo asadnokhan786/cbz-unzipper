0ce9143e89eaea4053067d7f0897a2fdc6ded7e5
asadnokhan786 <asadnokhan@outlook.com>
2024-10-23 23:28:12 -0600

    Added driver zip functionallity
    Added the zipping functionallity in main.py as well as the corresponding test cases in test_main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from unzip import unzip_all
+from zip import zip_all
 
 
 def main():
@@ -9,17 +10,23 @@ def main():
                                                   aimed at unzipping/zipping files in mass safely and reliably to the user\'s specific needs')
     parser.add_argument('--src', '-s', required='true', type=str, help='Source file path, required')
     parser.add_argument('--dst', '-d', required='true', type=str, help='Destination file path, required')
+    parser.add_argument('--mode', '-m', required='true', type=str, help='Mode to specify unzipping or zipping')
 
     args = parser.parse_args()
 
     base_dir = os.path.abspath(os.path.dirname(__file__))
     src = os.path.join(base_dir, args.src)
     dst = os.path.join(base_dir, args.dst)
+    mode = args.mode
 
     if not os.path.isdir(src):
         raise ValueError(f"{src} is NOT a valid source path")
     
-    unzip_all(src, dst)
+    if mode == "unzip":
+        unzip_all(src, dst)
+
+    if mode == 'zip':
+        zip_all(src, dst)
 
 
 if __name__ == '__main__':
